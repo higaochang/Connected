1ecb97d4b785e21f88d1b66c853e4a4d2916f7d8
chang <higaochang@gmail.com>
2018-03-01 16:51:20 +0800

    can read file

--- src/com/fenics/interview/Main.java
+++ src/com/fenics/interview/Main.java
@@ -1,11 +1,42 @@
 package com.fenics.interview;
 
-import sun.lwawt.macosx.CImage;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
+        if (args.length < 3){
+            System.out.println("please run the program using params as \"filename fromCity toCity\"");
+            return;
+        }
+
+        CityGraph<City> graph = CityGraph.newInstance();
+
+        String fileName = args[0];
+        String line = null;
+        try {
+            FileReader fileReader = new FileReader("./" + fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                String[] arr = line.split(",");
+                graph.addEdge(new City(arr[0].trim()), new City(arr[1].trim()));
+            }
+
+            bufferedReader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");
+        } catch (IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
+        if (graph.isConnected(new City(args[1]), new City(args[2]))){
+            System.out.println("yes");
+        }else {
+            System.out.println("no");
+        }
+
 
     }
 
